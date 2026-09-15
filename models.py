43f1b9196cb850630ef597e58bc1c45bc7f41273
Arthur Silva <silvaarthur31807@gmail.com>
2026-09-15 14:28:53 -0300

    models 1
    class usuario(db.model) e info do sql com id, nome, email e senha

---
+++ models.py
@@ -0,0 +1,12 @@
+from db import db
+
+class Usuario(db.Model):
+    table_name = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    senha = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Usuario {self.nome}>'
