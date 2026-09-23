34d8a2d19026731c622f4ed484fe641b12cd75de
Caiox67 <160370820+Caiox67@users.noreply.github.com>
2026-09-23 19:07:09 -0300

    Refactor button event listeners for visibility and accounts

--- templates/script.js
+++ templates/script.js
@@ -1,14 +1,52 @@
-let btnVisao = document.querySelector(".menu-topo a")
-let contador = 0
+const btnV = document.querySelector("#visao")
+const btnC = document.querySelector("#contas")
+const btnT = document.querySelector("#trs")
 
-btnVisao.onclick = function visaoGeral(){
-    document.querySelector("a.active").classList.remove("active")
-    
-    if(contador < 2){
-        contador = contador + 1
-    }else{
-        contador = 0
-    }
+btnV.addEventListener('click', function() {
+    btnV.classList.add('active');
 
-    btnVisao[contador].classList.add("active")
-}
+    btnC.classList.remove('active');
+    btnT.classList.remove('active');
+
+});
+
+btnC.addEventListener('click', function() {
+    btnC.classList.add('active');
+
+    btnV.classList.remove('active');
+    btnT.classList.remove('active');
+});
+
+btnT.addEventListener('click', function() {
+    btnT.classList.add('active');
+
+    btnV.classList.remove('active');
+    btnC.classList.remove('active');
+});
+
+
+const btnTd= document.querySelector("#todos")
+const btnPJ = document.querySelector("#PJ")
+const btnPF = document.querySelector("#PF")
+
+btnTd.addEventListener('click', function() {
+    btnTd.classList.add('active');
+
+    btnPJ.classList.remove('active');
+    btnPF.classList.remove('active');
+
+});
+
+btnPJ.addEventListener('click', function() {
+    btnPJ.classList.add('active');
+
+    btnTd.classList.remove('active');
+    btnPF.classList.remove('active');
+});
+
+btnPF.addEventListener('click', function() {
+    btnPF.classList.add('active');
+
+    btnPJ.classList.remove('active');
+    btnPF.classList.remove('active');
+});
